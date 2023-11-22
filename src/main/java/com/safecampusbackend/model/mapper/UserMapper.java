0dccf05623a54679259313b55b5135e018e72d67
ahaocoder <ahao.coder@gmail.com>
2023-11-22 17:16:03 +0800

    fix：RegisterMapper->UserMapper

--- src/main/java/com/safecampusbackend/model/mapper/UserMapper.java
+++ src/main/java/com/safecampusbackend/model/mapper/UserMapper.java
@@ -3,5 +3,5 @@ package com.safecampusbackend.model.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.safecampusbackend.model.entity.UserEntity;
 
-public interface RegisterMapper extends BaseMapper<UserEntity> {
+public interface UserMapper extends BaseMapper<UserEntity> {
 }
