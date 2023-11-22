e53de9bfb98bbbb23469cd47381fa79583b8dbbd
ahaocoder <ahao.coder@gmail.com>
2023-11-22 18:52:39 +0800

    fix：删除邮件的测试

--- src/main/java/com/safecampusbackend/SafeCampusBackendApplication.java
+++ src/main/java/com/safecampusbackend/SafeCampusBackendApplication.java
@@ -1,6 +1,5 @@
 package com.safecampusbackend;
 
-import cn.hutool.extra.mail.MailUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +10,7 @@ public class SafeCampusBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SafeCampusBackendApplication.class, args);
-        MailUtil.send("yohoozhao@163.com", "测试邮件工具", "Hello World", false);
+//        MailUtil.send("yohoozhao@163.com", "测试邮件工具", "Hello World", false);
         System.out.println("------------------------安全校园，启动！------------------------");
     }
 
